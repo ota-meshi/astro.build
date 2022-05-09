c8f1589d0680f3c28e05cdbfa6e72da4bd497885
yosuke ota <otameshiyo23@gmail.com>
2022-05-09 11:50:19 +0900

    Use eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['@typescript-eslint', 'prettier'],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/camelcase': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-this-alias': 'off',
+		'no-console': 'warn',
+		'prefer-const': 'off',
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+		// 'prettier/prettier': 'error',
+	},
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
+}
